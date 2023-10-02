e2451bc9a18873f4be1ad333f3b4cd4f2e1b7240
Aaron <70384942+MathsPsychopath@users.noreply.github.com>
2023-10-02 19:44:36 +0100

    Completed Week2

--- Week1/extras/RedNoise/src/RedNoise.cpp
+++ Week1/extras/RedNoise/src/RedNoise.cpp
@@ -3,17 +3,50 @@
 #include <Utils.h>
 #include <fstream>
 #include <vector>
+#include <glm/glm.hpp>
 
 #define WIDTH 320
 #define HEIGHT 240
 
+std::vector<float> interpolateSingleFloats(float from, float to, int numberOfValues) {
+	const float increment = (to - from) / (numberOfValues - 1);
+	std::vector<float> output = {};
+	for (int i = 0; i < numberOfValues; i++) {
+		output.push_back(from + i * increment);
+	}
+	return output;
+}
+
+std::vector<glm::vec3> interpolateThreeElementValues(glm::vec3 from, glm::vec3 to, int numberOfValues) {
+	const int indices = numberOfValues - 1;
+	glm::vec3 steps((to.x - from.x) / indices, (to.y - from.y) / indices, (to.z - from.z) / indices);
+	std::vector<glm::vec3> output = {};
+	for (int i = 0; i < numberOfValues; i++) {
+		output.push_back(from + (steps * float(i)));
+	}
+	return output;
+}
+
 void draw(DrawingWindow &window) {
 	window.clearPixels();
+	glm::vec3 topLeft(255, 0, 0);
+	glm::vec3 topRight(0, 0, 255);
+	glm::vec3 bottomRight(0, 255, 0);
+	glm::vec3 bottomLeft(255, 255, 0);
+
+	std::vector<glm::vec3> startColumnInterpolation = 
+		interpolateThreeElementValues(topLeft, bottomLeft, window.height);
+	std::vector<glm::vec3> endColumnInterpolation =
+		interpolateThreeElementValues(topRight, bottomRight, window.height);
+
 	for (size_t y = 0; y < window.height; y++) {
+		std::vector<glm::vec3> rowInterpolation = 
+			interpolateThreeElementValues(startColumnInterpolation[y], endColumnInterpolation[y], window.width);
 		for (size_t x = 0; x < window.width; x++) {
-			float red = rand() % 256;
-			float green = 0.0;
-			float blue = 0.0;
+			glm::vec3 currentPixel = rowInterpolation[x];
+			float red = currentPixel.x;
+			float green = currentPixel.y;
+			float blue = currentPixel.z;
 			uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
 			window.setPixelColour(x, y, colour);
 		}
@@ -35,6 +68,14 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 int main(int argc, char *argv[]) {
 	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 	SDL_Event event;
+
+	/*std::vector<glm::vec3> result;
+	glm::vec3 from(1.0, 4.0, 9.2);
+	glm::vec3 to(4.0, 1.0, 9.8);
+	result = interpolateThreeElementValues(from, to, 4);
+	for (size_t i = 0; i < result.size(); i++) std::cout << "(" << result[i].x << ", " << result[i].y << ", " << result[i].z << ") ";
+	std::cout << std::endl;*/
+
 	while (true) {
 		// We MUST poll for events - otherwise the window will freeze !
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
